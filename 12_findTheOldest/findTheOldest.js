248f9fbb35a4fd80b3a0dea7e2c40215e89c8304
chdowen <christopherdowen@gmail.com>
2024-11-13 16:45:42 -0800

    Update findTheOldest.js

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,19 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    const ages = [];
 
+    function getAge(birth, death){
+        if(!death){
+            death = new Date().getFullYear();
+        }
+        age = death - birth;
+        return age;
+    }
+
+    
 };
-//date.getFullYear();
+/*
+if people[i].yearOfDeath = undefined, then
+people[i].yearOfDeath =  date.getFullYear();
+*/
 // Do not edit below this line
 module.exports = findTheOldest;
